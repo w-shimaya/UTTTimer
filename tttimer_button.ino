b0bfc10f775d50899b03ed1e2523f541f530394d
showyama <pptt03005@gmail.com>
2020-03-22 17:32:45 +0900

    add bottun control

--- tttimer_button.ino
+++ tttimer_button.ino
@@ -1,4 +1,8 @@
 #define SET(n) digitalWrite((n), LOW)
+#define READY 0
+#define COUNT 1
+#define STOP 2
+#define PUSH_SHORT 100
 
 const int dig[4] = {2, 5, 6, 14};
 
@@ -13,9 +17,18 @@ const int dp = 17;
 
 const int spk = 10;
 
-unsigned long time_milli_start = 0;
-unsigned long time_milli_end = 0;
+const int sw_one = 8;
+const int sw_two = 9;
+
+unsigned long time_milli_start = 0; //カウントが始まった時刻
+unsigned long time_milli_now = 0; //表示する時刻
+unsigned long time_milli_stop = 0; //停止時刻
 unsigned long alarm_start_time = 0;
+
+int state = 0; //今の状態
+
+int count_low_one = 0; //チャタリングに使うカウント
+int count_low_two = 0;
 
 // 全てのDIG(桁)をクリア
 void clearDigit(){
@@ -36,7 +49,7 @@ void clearSegment(){
 }
 
 void setup() {
-  // 全てのピンを出力にする
+  // 7セグとブザーのピンを出力にする
   pinMode(dig[0], OUTPUT);
   pinMode(dig[1], OUTPUT);
   pinMode(dig[2], OUTPUT);
@@ -51,6 +64,10 @@ void setup() {
   pinMode(dp, OUTPUT);
   pinMode(spk, OUTPUT);
 
+  //スイッチのピンの設定
+  pinMode(sw_one,INPUT_PULLUP);
+  pinMode(sw_two,INPUT_PULLUP);
+
   // 全てのDIG(桁)をクリア
   clearDigit();
 
@@ -58,7 +75,11 @@ void setup() {
   clearSegment();
 
   time_milli_start = millis();
-  time_milli_end = millis();
+  time_milli_now = millis();
+  time_milli_stop = 0;
+
+  state = READY;
+
 }
 
 void show_num(int* n) {
@@ -125,7 +146,90 @@ void alarm(int on_time,int off_time, int repeat, unsigned long alarm_start_time)
 }
 
 
+
 void loop() {
+  switch(state){
+    case READY: //READYのとき，表示は全て０
+      int num[4] = {};
+      show_num(num);
+
+      //スイッチ１(start/stop)が押されたとき，その時刻をtime_milli_startにしてCOUNTへ
+      if (digitalRead(sw_one) == LOW){
+        if(count_low_one <= PUSH_SHORT)count_low_one++;
+      }else{
+        count_low_one = 0;
+      }
+      if(count_low_one == PUSH_SHORT){
+        time_milli_start = millis();
+        state = COUNT;
+      }
+      break;
+
+    case COUNT: //COUNTのとき，カウントアップ
+      time_milli_now = millis() - time_milli_start + time_milli_stop;
+      int delta = time_milli_now / 1000; //second
+      int now_sec = delta % 60;
+      int now_min = delta / 60;
+      int num[4];
+      num[0] = now_min / 10;
+      num[1] = now_min % 10;
+      num[2] = now_sec / 10;
+      num[3] = now_sec % 10;
+      show_num(num);
+
+      //スイッチ１(start/stop)が押されたとき，表示されている時刻をtime_milli_stopに記録してSTOPへ
+      if (digitalRead(sw_one) == LOW){
+        if(count_low_one <= PUSH_SHORT)count_low_one++;
+      }else{
+        count_low_one = 0;
+      }
+      if(count_low_one == PUSH_SHORT){
+        time_milli_stop = time_milli_now;
+        state = STOP;
+      }
+      break;
+
+    case STOP: //STOPのとき，カウント停止．time_milli_stopを表示
+      int delta = time_milli_stop / 1000;
+      int now_sec = delta % 60;
+      int now_min = delta / 60;
+
+      int num[4];
+      num[0] = now_min / 10;
+      num[1] = now_min % 10;
+      num[2] = now_sec / 10;
+      num[3] = now_sec % 10;
+      show_num(num);
+
+      //スイッチ1(start/stop)が押されたとき，その時刻をtime_milli_startにしてCOUNTへ
+      if (digitalRead(sw_one) == LOW){
+        if(count_low_one <= PUSH_SHORT)count_low_one++;
+      }else{
+        count_low_one = 0;
+      }
+      if(count_low_one == PUSH_SHORT){
+        time_milli_start = millis();
+        state = COUNT;
+      }
+
+      //スイッチ2(reset)が押されたとき，time_milli_stopを0にしてREADYへ
+      if (digitalRead(sw_two) == LOW){
+        if(count_low_two <= PUSH_SHORT)count_low_two++;
+      }else{
+        count_low_two = 0;
+      }
+      if(count_low_two == PUSH_SHORT){
+        time_milli_stop = 0;
+        state = READY;
+      }
+
+      break;
+
+  }
+
+
+/*
+
   time_milli_end = millis();
   int delta = (time_milli_end - time_milli_start) / 1000; // second
   int now_sec = delta % 60;
@@ -146,5 +250,7 @@ void loop() {
   }
 
   alarm(200,50,4,alarm_start_time);
+
+*/
 
 }
